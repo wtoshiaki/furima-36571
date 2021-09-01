ccebf785dcb465ac7c3d0c9aed808232729e3376
wtoshiaki <88226506+wtoshiaki@users.noreply.github.com>
2021-09-01 18:31:05 +0900

    ユーザー管理機能　修正#3

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-             
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
+    validates :first_name_kana 
+    validates :last_name_kana
+  end  
   validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i} 
   validates :nickname, presence: true
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+ 
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+
   validates :birthday, presence: true
   
 
@@ -17,3 +21,4 @@ class User < ApplicationRecord
   #has_many :orders
 
 end
+
